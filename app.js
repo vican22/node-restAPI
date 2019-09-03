c7aefecd50caea0ff4535bd4dfb90910571e0a47
Vedran <vedranknezevic1@yahoo.com>
2019-09-03 20:11:25 +0200

    createPost()

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const graphqlHttp = require("express-graphql");
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
 
+const auth = require("./middleware/auth");
+
 const mongoURI = require("./config/keys").MONGO_URI;
 
 const app = express();
@@ -57,6 +59,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth);
 
 app.use(
   "/graphql",
